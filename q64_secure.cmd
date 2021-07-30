bddeb7d117d20efa37f8c26a7866343923d4799d
Rickey Bowers Jr <bitRAKE@gmail.com>
2021-07-30 14:31:37 -0600

    Update q64_secure.cmd

--- q64_secure.cmd
+++ q64_secure.cmd
@@ -1,4 +1,3 @@
-@echo on
 "C:\Program Files\qemu\qemu-system-x86_64w.exe" ^
  -nodefaults ^
  -cpu qemu64 -vga std -machine q35,smm=on ^
@@ -6,3 +5,4 @@
  -L . -drive file=fat:rw:image,index=1,media=disk,if=virtio,format=raw ^
  -drive file=firmware\OVMF_amd64\OVMF_CODE_4M.ms.fd,if=pflash,format=raw,unit=0,readonly=on ^
  -drive file=firmware\OVMF_amd64\OVMF_VARS_4M.ms.fd,if=pflash,format=raw,unit=1
+ 
